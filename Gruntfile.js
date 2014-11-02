2c9b600abe97d6ba1cd020f8ef61522d13e07053
pdt <thanh.phan@outlook.com>
2014-11-02 20:19:41 +0100

    setup bower & grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            src: {
+                files: ['**/*.scss', '**/*.html'],
+                tasks: ['compass:dev'] 
+            },
+           options: {
+                livereload: true,
+            },
+        },
+        compass: {
+            dev: {
+                options: {
+                    sassDir: 'styleproject/sass',
+                    cssDir: 'hackernews/static/css',
+                    imagesPath: 'img',
+                    noLineComments: false,
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+};
